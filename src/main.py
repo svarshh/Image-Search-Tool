a6e313d2f93823fa450fbaa000c605e299083829
Varsha <svarshh@users.noreply.github.com>
2025-10-03 22:05:31 -0700

    added the functionality to find files containing given search string

--- src/main.py
+++ src/main.py
@@ -20,27 +20,48 @@ def get_images(path):
     for root, _, files in os.walk(path):
         for f in files:
             if f.endswith('.png') or f.endswith('jpeg') or f.endswith('.jpg'):
-                print(os.path.join(root, f))
                 images.append(os.path.join(root, f))
     return images
     
+def store_image_data(path):
+    
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["mydatabase"]
+    collection = db["texts"]
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["mydatabase"]
-collection = db["texts"]
+    images = get_images(r'.')
 
-images = get_images(r'.')
+    for image in images:
+        sha = get_sha256(image)
+        doc = list(collection.find({'_id':sha}))
+        if not doc:
+            text_data = pytesseract.image_to_string(Image.open(image))
+            document = {
+                "_id" : sha,
+                "file_name" : image,
+                "data": text_data
+            }
+            collection.insert_one(document)
 
-for image in images:
-    sha = get_sha256(image)
-    doc = list(collection.find({'_id':sha}))
-    if not doc:
-        text_data = pytesseract.image_to_string(Image.open(image))
-        document = {
-            "_id" : sha,
-            "file_name" : image,
-            "data": text_data
-        }
-        collection.insert_one(document)
+def check_match(match):
 
-print(client.list_database_names())
+    paths = []
+
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["mydatabase"]
+    collection = db["texts"]
+    images = list(collection.find())
+
+    import re
+    for i in images:
+        if(re.search(match, i['data'], re.IGNORECASE)):
+            paths.append(i['file_name'])
+            
+    return paths
+
+
+
+store_image_data(r'.')
+paths = check_match("io")
+for p in paths:
+    print(p)
